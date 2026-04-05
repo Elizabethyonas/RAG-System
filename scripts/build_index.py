41c3364367decf1cb2fc71640393e57c4f3a4599
Elizabethyonas <elizabethyonas700@gmail.com>
2026-04-05 20:47:27 +0300

    init

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -2,6 +2,13 @@ from __future__ import annotations
 
 import os
 
+os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "TRUE")
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+
+import torch
+
+torch.set_num_threads(1)
+
 import faiss
 import numpy as np
 from sentence_transformers import SentenceTransformer
